a43f79b0345eb085172c50a89ad03f71f0227d6c
SUNNY <hotsummer2626@gmail.com>
2021-05-19 18:44:59 +1000

    react life prac

--- react/index.js
+++ react/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-ReactDom.render(
-    (
-        <div id="my-div">
-            <p>Hello world</p>
-            <a href="https://google.com">Google</a>
-        </div>
-    ),
-    document.querySelector('#app'),
-);
+class Life extends React.Component {
+    state = {
+        opacity: 0.5
+    }
+
+    death = () => {
+        ReactDom.unmountComponentAtNode(document.querySelector('#app'));
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(() => {
+            let opacity = this.state.opacity;
+            opacity -= 0.1;
+            if(opacity<= 0) opacity = 1;
+            this.setState({opacity});
+        }, 200)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timer);
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 style={{ opacity: this.state.opacity }}>hhhhhhhhhhh</h2>
+                <button onClick={this.death}>cancel</button>
+            </div>
+        )
+    };
+}
+
+
+ReactDom.render((<Life />), document.querySelector('#app'));
+
+
+
+
