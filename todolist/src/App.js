039fe30c2c00362a11216487b588a6909d246ff6
SUNNY <hotsummer2626@gmail.com>
2021-05-24 03:00:09 +1000

    finish todolist

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -14,14 +14,52 @@ export default class App extends Component {
     ]
   };
 
+  addTodo = (todoObj) => {
+    const { todos } = this.state;
+    const newTodos = [todoObj, ...todos];
+    this.setState({ todos: newTodos });
+  }
+
+  updateTodo = (id, done) => {
+    const { todos } = this.state;
+    const newTodos = todos.map((todoObj) => {
+      return todoObj.id === id ? { ...todoObj, done } : todoObj;
+    })
+    this.setState({ todos: newTodos });
+  }
+
+  deleteTodo = (id) => {
+    const { todos } = this.state;
+    const newTodos = todos.filter((todoObj) => {
+      return todoObj.id !== id;
+    })
+    this.setState({ todos: newTodos });
+  }
+
+  checkAllTodo = (done) => {
+    const { todos } = this.state;
+    const newTodos = todos.map((todoObj) => {
+      return { ...todoObj, done };
+    })
+    this.setState({ todos: newTodos });
+  }
+
+  clearAllDone = () => {
+    const { todos } = this.state;
+    const newTodos = todos.filter((todoObj) => {
+      return !todoObj.done;
+    })
+    this.setState({ todos: newTodos });
+  }
+
   render() {
     const { todos } = this.state;
 
     return (
       <div className="container">
-        <Header />
-        <List todos={todos} />
-        <Footer />
+        <Header addTodo={this.addTodo} />
+        <List todos={todos} updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} />
+        <Footer todos={todos} checkAllTodo={this.checkAllTodo} clearAllDone={this.clearAllDone} />
       </div>
     )
   }
--- todolist/src/components/Header/index.js
+++ todolist/src/components/Header/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
 import './index.css';
 
 export default class Header extends Component {
+
+    static propTypes = {
+        addTodo: PropTypes.func.isRequired
+    }
+
+    handleKeyUp = (event) => {
+        const { keyCode, target } = event;
+        if (keyCode !== 13) return;
+        if (target.value.trim() === '') {
+            alert('input cannot be empty!!!');
+            return;
+        }
+        const todoObj = { id: nanoid(), name: target.value, done: false };
+        this.props.addTodo(todoObj);
+        target.value = '';
+    }
+
     render() {
         return (
             <div className="header">
-                <input type="text" placeholder="Please input name and click enter" />
+                <input onKeyUp={this.handleKeyUp} type="text" placeholder="Please input name and click enter" />
             </div>
         )
     }
