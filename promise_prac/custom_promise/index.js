a1ce456b19df9016246de4b3f041939863a27bf3
SUNNY <hotsummer2626@gmail.com>
2021-05-27 01:55:33 +1000

    add catch resolve reject all and race functions into promise

--- promise_prac/custom_promise/index.js
+++ promise_prac/custom_promise/index.js
@@ -10,9 +10,12 @@ function Promise(executor) {
         if (self.PromiseState !== 'pending') return;
         self.PromiseState = 'fulfilled';
         self.PromiseResult = date;
-        self.callback.forEach(item => {
-            item.onResolve(date);
-        });
+        setTimeout(() => {
+            self.callback.forEach(item => {
+                item.onResolve(date);
+            });
+        })
+
 
     }
 
@@ -20,9 +23,11 @@ function Promise(executor) {
         if (self.PromiseState !== 'pending') return;
         self.PromiseState = 'rejected';
         self.PromiseResult = date;
-        self.callback.forEach(item => {
-            item.onReject(date);
-        });
+        setTimeout(() => {
+            self.callback.forEach(item => {
+                item.onReject(date);
+            });
+        })
     }
 
     try {
@@ -35,8 +40,19 @@ function Promise(executor) {
 
 Promise.prototype.then = function (onResolve, onReject) {
     const self = this;
+
+    if (typeof onResolve !== 'function') {
+        onResolve = value => value;
+    }
+
+    if (typeof onReject !== 'function') {
+        onReject = reason => {
+            throw reason;
+        }
+    }
+
     return new Promise((resolve, reject) => {
-        function callback(type){
+        function callback(type) {
             try {
                 let result = type(self.PromiseResult);
                 if (result instanceof Promise) {
@@ -53,10 +69,14 @@ Promise.prototype.then = function (onResolve, onReject) {
             }
         }
         if (this.PromiseState === 'fulfilled') {
-            callback(onResolve);
+            setTimeout(() => {
+                callback(onResolve);
+            })
         }
         if (this.PromiseState === 'rejected') {
-            callback(onReject);
+            setTimeout(() => {
+                callback(onReject);
+            })
         }
         if (this.PromiseState === 'pending') {
             this.callback.push({
@@ -67,6 +87,60 @@ Promise.prototype.then = function (onResolve, onReject) {
                     callback(onReject);
                 }
             });
+        }
+    })
+}
+
+Promise.prototype.catch = function (onReject) {
+    return this.then(undefined, onReject);
+}
+
+Promise.resolve = function (value) {
+    return new Promise((resolve, reject) => {
+        if (value instanceof Promise) {
+            value.then(v => {
+                resolve(v);
+            }, r => {
+                reject(r);
+            })
+        } else {
+            resolve(value);
+        }
+    })
+}
+
+Promise.reject = function (reason) {
+    return new Promise((resolve, reject) => {
+        reject(reason);
+    })
+}
+
+Promise.all = function (promises) {
+    return new Promise((resolve, reject) => {
+        let count = 0;
+        let arr = [];
+        for (let i = 0; i < promises.length; i++) {
+            promises[i].then(v => {
+                count++;
+                arr[i] = v;
+                if (count === promises.length) {
+                    resolve(arr);
+                }
+            }, r => {
+                reject(r);
+            });
+        }
+    })
+}
+
+Promise.race = function (promises) {
+    return new Promise((resolve, reject) => {
+        for (let i = 0; i < promises.length; i++) {
+            promises[i].then(v => {
+                resolve(v);
+            }, r => {
+                reject(r);
+            })
         }
     })
 }
